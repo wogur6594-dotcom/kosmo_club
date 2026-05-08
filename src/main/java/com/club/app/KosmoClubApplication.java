f61d98fd6847c5dd86b0cf659757b19215744d1b
semingit <semin0216@naver.com>
2026-05-08 18:01:35 +0900

    프로젝트 시작

---
+++ src/main/java/com/club/app/KosmoClubApplication.java
@@ -0,0 +1,13 @@
+package com.club.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KosmoClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KosmoClubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/semin/app/KosmoClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semin.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KosmoClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
