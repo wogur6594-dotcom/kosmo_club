c815e2f058029760e6e99fe2efcad4c563d37460
wogur6594-dotcom <wogur6594@gmail.com>
2026-05-26 18:14:05 +0900

    ..

--- src/main/java/com/club/app/configs/SecurityConfig.java
+++ src/main/java/com/club/app/configs/SecurityConfig.java
@@ -4,7 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.session.HttpSessionEventPublisher;
 
 import com.club.app.security.LoginFailHandler;
 
@@ -12,105 +14,64 @@ import com.club.app.security.LoginFailHandler;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    private final LoginFailHandler loginFailHandler;
+	private final LoginFailHandler loginFailHandler;
 
-    SecurityConfig(LoginFailHandler loginFailHandler) {
-        this.loginFailHandler = loginFailHandler;
-    }
+	SecurityConfig(LoginFailHandler loginFailHandler) {
+		this.loginFailHandler = loginFailHandler;
+	}
 
-    @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http
-            .cors(cors -> {
-                cors.disable();
-            })
-            .csrf(csrf -> {
-                csrf.disable();
-            })
-            .authorizeHttpRequests(auth -> {
+		http.cors(cors -> {
+			cors.disable();
+		}).csrf(csrf -> {
+			csrf.disable();
+		}).authorizeHttpRequests(auth -> {
 
-                auth
-                    // 로그인 필요한 페이지
-                    .requestMatchers(
-                        "/member/update",
-                        "/member/detail",
-                        "/member/delete",
-                        "/member/pwChange",
-                        "/member/mypage",
-                        "/product/add",
-                        "/product/detail",
-                        "/product/myList",
-                        "/product/delete",
-                        "/product/edit",
-                        "/product/addFile",
-                        "/product/deleteFile",
-                        "/job/create",
-                        "/job/create/**",
-                        "/job/update",
-                        "/job/update/**",
-                        "/job/delete",
-                        "/job/delete/**",
-                        "/job/deleteFile",
-                        "/job/deleteFile/**",
-                        "/job/myJobList",
-                        "/jobApply/**",
+			auth
+					// 로그인 필요한 페이지
+					.requestMatchers("/member/update", "/member/detail", "/member/delete", "/member/pwChange",
+							"/member/mypage", "/product/add", "/product/detail", "/product/myList", "/product/delete",
+							"/product/edit", "/product/addFile", "/product/deleteFile", "/job/create", "/job/create/**",
+							"/job/update", "/job/update/**", "/job/delete", "/job/delete/**", "/job/deleteFile",
+							"/job/deleteFile/**", "/job/myJobList", "/jobApply/**",
 
-                        "/restaurant/create",
-                        "/restaurant/create/**",
+							"/restaurant/create", "/restaurant/create/**",
 
-                        "/club/create",
-                        "/club/create/**",
-                        "/clubSchedule/list",
-                        "/clubSchedule/create",
-                        "/clubSchedule/create/**",
-                        "/productChat/**"
-                    ).authenticated()
+							"/club/create", "/club/create/**", "/clubSchedule/list", "/clubSchedule/create",
+							"/clubSchedule/create/**", "/productChat/**")
+					.authenticated()
 
-                    // 모두 허용
-                    .requestMatchers(
-                        "/",
-                        "/member/login",
-                        "/member/join",
-                        "/product/list",
-                        "/css/**",
-                        "/js/**",
-                        "/images/**",
-                        "/files/**"
-                    ).permitAll()
+					// 모두 허용
+					.requestMatchers("/", "/member/login", "/member/join", "/product/list", "/css/**", "/js/**",
+							"/images/**", "/files/**")
+					.permitAll()
 
-                    .anyRequest().permitAll();
-            })
+					.anyRequest().permitAll();
+		})
 
-            .formLogin(login -> {
+				.formLogin(login -> {
+					login.loginPage("/member/login").usernameParameter("memberId").passwordParameter("memberPw")
+							.loginProcessingUrl("/member/login").defaultSuccessUrl("/", true)
+							.failureHandler(loginFailHandler).permitAll();
+				})
 
-                login
-                    .loginPage("/member/login")
-                    .usernameParameter("memberId")
-                    .passwordParameter("memberPw")
-                    .loginProcessingUrl("/member/login")
-                    .defaultSuccessUrl("/")
-                    .failureHandler(loginFailHandler)
-                    .permitAll();
-            })
+				.rememberMe(remember -> remember.rememberMeParameter("remember-me")
+						.tokenValiditySeconds(60 * 60 * 24 * 7).key("myRememberKey"))
 
-            .rememberMe(remember ->
+				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
 
-                remember
-                    .rememberMeParameter("remember-me")
-                    .tokenValiditySeconds(60 * 60 * 24 * 7)
-                    .key("myRememberKey")
-            )
+				.logout(logout -> {
+					logout.logoutUrl("/member/logout").invalidateHttpSession(true).deleteCookies("JSESSIONID")
+							.logoutSuccessUrl("/");
+				});
 
-            .logout(logout -> {
+		return http.build();
+	}
 
-                logout
-                    .logoutUrl("/member/logout")
-                    .invalidateHttpSession(true)
-                    .deleteCookies("JSESSIONID")
-                    .logoutSuccessUrl("/");
-            });
-
-        return http.build();
-    }
+	@Bean
+	public HttpSessionEventPublisher httpSessionEventPublisher() {
+		return new HttpSessionEventPublisher();
+	}
 }
